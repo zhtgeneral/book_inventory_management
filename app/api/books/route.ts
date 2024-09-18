559e50823896e2bc8a96b6b13162520c2c690ca4
Zhong Tan <zhtgeneral@gmail.com>
2024-09-18 11:25:17 -0700

    added docs to endpoints

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -3,8 +3,19 @@ import { Book } from "@/types";
 import { NextRequest, NextResponse } from "next/server";
 import { Genre as PrismaGenre }  from '@prisma/client';
 
-
-export async function POST(request: NextRequest) {
+/**
+ * API endpoint that adds book to the collection of books.
+ * 
+ * If the data is invalid: Either the title length is > 100 or the author length is > 40, 
+ * then throw a 400 error for Invalid Data
+ * otherwise create book into database.
+ * 
+ * If database error occurs, throw a 500 error for internal error.
+ * 
+ * @param request The request of data. The format is expected to be type `Book`
+ * @returns json of the newly added book or an error
+ */
+export async function POST(request: NextRequest): Promise<NextResponse> {
   try {
     const body: Book = await request.json();
     const { id, title, author, genre, publication_date, ISBN } = body;
@@ -28,7 +39,13 @@ export async function POST(request: NextRequest) {
   }
 }
 
-export async function GET() {
+/**
+ * Gets all the books from the database.
+ * 
+ * If database error occurs, throw a 500 error for internal error.
+ * @returns the books stored in the database as an array of `Book[]`
+ */
+export async function GET(): Promise<NextResponse> {
   try {
     const books: Book[] = await prisma.book.findMany({});
     return NextResponse.json(books);
