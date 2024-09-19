10da33ea0be54be8c172073f36cba4104ed7e8fc
Zhong Tan <zhtgeneral@gmail.com>
2024-09-19 16:59:26 -0700

    added more front end validation. TODO hydration error

--- app/utils/zod.ts
+++ app/utils/zod.ts
@@ -1,22 +1,30 @@
 import { Genre } from "@prisma/client";
-import { z, ZodObject } from "zod";
+import { z, ZodObject, ZodString } from "zod";
 
 export const formSchema: ZodObject<{
   title: z.ZodString;
   author: z.ZodString;
   genre: z.ZodEnum<[string, ...string[]]>;
   publication_date: z.ZodDate;
-  ISBN: z.ZodString;
+  ISBN: z.ZodEffects<ZodString, string, string>; 
 }> = z.object({
-  title: z.string().max(100, {
+  title: z.string().min(1, {
+    message: "Title must be at least 1 character"
+  }).max(100, {
     message: "Title can't exceed 100 characters"
   }),
-  author: z.string().max(40, {
+  author: z.string().min(1, {
+    message: "Author must be at least 1 character"
+  }).max(40, {
     message: "Author can't exceed 40 characters"
   }),
   genre: z.enum(Object.values(Genre) as [string, ...string[]], {
-    message: "Expected one of the provided genres"
+    message: "Genre must be one of the provided genres"
   }),
   publication_date: z.date(),
-  ISBN: z.string()
+  ISBN: z.string().refine((isbn: string) => {
+    return isbn.length === 10 || isbn.length === 13;
+  }, {
+    message: "ISBN must be 10 or 13 characters"
+  })
 })
