1ca9b3268aa65cc45c9d488d6610ec023f885f5f
Zhong Tan <zhtgeneral@gmail.com>
2024-09-19 16:50:08 -0700

    comments. client validation for form TODO

--- app/utils/getBooks.ts
+++ app/utils/getBooks.ts
@@ -5,7 +5,7 @@ import { SetStateAction } from "react";
 /**
  * Fetches all books from database.
  * 
- * If error occurs, throw it and handle accordingly in client
+ * If error occurs, handle the error here. Errors can't make it into client.
  * @param callback function that handles the newly obtained books
  */
 export default async function getBooks(callback: (value: SetStateAction<Inventory[]>) => void) {
